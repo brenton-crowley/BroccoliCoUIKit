314c19833f26d74063d944979d6d280edca24144
brenton-crowley <brenton.crowley@gmail.com>
2022-11-05 13:13:24 +1100

    add registerButton target

--- BroccoliCoUIKit/Features/RegisterForm/RegisterFormViewController.swift
+++ BroccoliCoUIKit/Features/RegisterForm/RegisterFormViewController.swift
@@ -19,9 +19,11 @@ class RegisterFormViewController: UIViewController, UITableViewDelegate, UITable
     
     private struct Constants {
         static let buttonHeight:CGFloat = 50.0
+        static let xOffset:CGFloat = 20.0
     }
     
-    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private var tableView = UITableView(frame: .zero, style: .grouped)
+    private var sendButton = UIButton()
     private var observer: NSObjectProtocol?
     
     var fields: [TextInputField] = [
@@ -44,7 +46,8 @@ class RegisterFormViewController: UIViewController, UITableViewDelegate, UITable
         
         // Do any additional setup after loading the view.
         NSLayoutConstraint.activate(
-            setupTableView()
+            setupTableView() +
+            setupButton()
         )
     }
     
@@ -74,9 +77,40 @@ class RegisterFormViewController: UIViewController, UITableViewDelegate, UITable
         return constaints
     }
     
+    private func setupButton() -> [NSLayoutConstraint] {
+        
+        var config = UIButton.Configuration.filled()
+        config.buttonSize = .large
+        config.cornerStyle = .capsule
+        
+        sendButton.configuration = config
+        sendButton.tintColor = .themeAccent
+        sendButton.setTitle("Register my details", for: .normal)
+
+        sendButton.addTarget(self, action: #selector(registerDetailsTapped), for: .touchUpInside)
+        
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(sendButton)
+        
+        let constaints: [NSLayoutConstraint] = [
+            sendButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.buttonHeight - Constants.xOffset),
+            sendButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -Constants.xOffset),
+            sendButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Constants.xOffset),
+        ]
+        
+        return constaints
+        
+    }
+    
+    // MARK: - Send Button Target Action
+    @objc private func registerDetailsTapped() {
+        print("Register details - kickstart call to back end.")
+    }
+    
     // MARK: - UITableView Delegate
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50.0
+        return Constants.buttonHeight
     }
     
     // MARK: - UITableView DataSource
